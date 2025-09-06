c67dd45638f66eb008a6836c912ba6b76cb38ed7
Asmita Tagad <asmitatagad07@gmail.com>
2025-09-06 18:22:30 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api", userRoutes);
+
+// Start server
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { loginUser, registerUser } from "../controllers/user.js";
+
+
+const router = express.Router();
+
+// Register
+router.post("/register", registerUser);
+
+// Login
+router.post("/login", loginUser);
+
+export default router;
